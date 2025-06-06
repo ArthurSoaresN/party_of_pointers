5817a25ae4642a6c6c0c650071bfafc8c9c4e196
Arthur Soares <arthursoaressi@hotmail.com>
2025-06-06 09:36:33 -0300

    Function to fill list of names using pointer to pointers

--- main.c
+++ main.c
@@ -29,6 +29,18 @@ int division (int a, int b) {
 	return a / b;
 }
 
+void insert_names (char** names, int hmany) {
+	for(int i = 0; i < hmany; i++) {
+		names[i] = malloc(sizeof(char) * STRING_SIZE); 	
+	}
+	// test
+	strcpy(names[0], "MARIA");
+	strcpy(names[1], "JOSEPH");
+	strcpy(names[2], "JORGE");
+	strcpy(names[3], "ARTHUR");
+	strcpy(names[4], "LEVI");	
+}
+
 int main() {
 	
 	return 0;
