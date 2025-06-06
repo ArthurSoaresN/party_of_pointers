0f24ffca9aa00fc506779ff761eb00965100a190
Arthur Soares <arthursoaressi@hotmail.com>
2025-06-06 09:34:39 -0300

    Creating constants and importing libraries

--- main.c
+++ main.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct {
+	char* name; 
+	int age;
+} Person;
+
+#define STRING_SIZE 20
+#define ADD 0
+#define SUBTRACTION 1
+#define MULTIPLICATION 2
+#define DIVISION 3
 
 int main() {
 	
